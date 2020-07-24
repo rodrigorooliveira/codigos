043832b0dd2b4431a260096f7a6e3301cb4a84ed
rodrigo <rodrigorooliveira@gmail.com>
2020-07-24 17:28:59 -0300

    primeiro commit

---
+++ aulasphp/primeiro.php
@@ -0,0 +1,83 @@
+<?php
+// Conexão
+require_once 'db_connect.php';
+
+// Sessão
+session_start();
+
+// Botão enviar
+if(isset($_POST['btn-entrar'])):
+	$erros = array();
+	$login = mysqli_escape_string($connect, $_POST['login']);
+	$senha = mysqli_escape_string($connect, $_POST['senha']);
+
+	if(isset($_POST['lembrar-senha'])):
+
+		setcookie('login', $login, time()+3600);
+		setcookie('senha', md5($senha), time()+3600);
+	endif;
+
+	if(empty($login) or empty($senha)):
+		$erros[] = "<li> O campo login/senha precisa ser preenchido </li>";
+	else:
+		// 105 OR 1=1 
+	    // 1; DROP TABLE teste
+
+		$sql = "SELECT login FROM usuarios WHERE login = '$login'";
+		$resultado = mysqli_query($connect, $sql);		
+
+		if(mysqli_num_rows($resultado) > 0):
+		$senha = md5($senha);       
+		$sql = "SELECT * FROM usuarios WHERE login = '$login' AND senha = '$senha'";
+
+
+
+		$resultado = mysqli_query($connect, $sql);
+
+			if(mysqli_num_rows($resultado) == 1):
+				$dados = mysqli_fetch_array($resultado);
+				mysqli_close($connect);
+				$_SESSION['logado'] = true;
+				$_SESSION['id_usuario'] = $dados['id'];
+				header('Location: home.php');
+			else:
+				$erros[] = "<li> Usuário e senha não conferem </li>";
+			endif;
+
+		else:
+			$erros[] = "<li> Usuário inexistente </li>";
+		endif;
+
+	endif;
+
+endif;
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Olá, mundo!</title>
+<body>
+<h1> Login </h1>
+<?php 
+if(!empty($erros)):
+	foreach($erros as $erro):
+		echo $erro;
+	endforeach;
+endif;
+?>
+<hr>
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+Login: <input type="text" name="login" value="<?php echo isset($_COOKIE['login']) ? $_COOKIE['login'] : '' ?>"><br>
+Senha: <input type="password" name="senha" value="<?php echo isset($_COOKIE['senha']) ? $_COOKIE['senha'] : '' ?>"><br>
+Lembrar senha: <input type="checkbox" name="lembrar-senha">
+<button type="submit" name="btn-entrar"> Entrar </button>
+</form>  
+</body>
+</html>
